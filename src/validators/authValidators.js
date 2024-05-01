03ba7577039f6691502af98d19fd9be44d1f7c91
aramind <88814622+aramind@users.noreply.github.com>
2024-05-02 00:50:00 +0800

    feat: adding the auth validators

---
+++ src/validators/authValidators.js
@@ -0,0 +1,76 @@
+const User = require("../models/User");
+
+const validRoles = ["admin", "editor", "viewer"];
+const validStatuses = ["active", "deactivated"];
+const authValidator = {
+  loginValidator: [
+    body("username").trim().notEmpty().withMessage("Username cannot be empty"),
+    body("password").notEmpty().withMessage("Password cannot be empty"),
+  ],
+  registerUserValidator: [
+    body("employeeId")
+      .trim()
+      .notEmpty()
+      .withMessage("Employee ID cannot be empty")
+      .bail()
+      .custom(async (employeeId) => {
+        const employeeIdExists = await User.findOne({ employeeId });
+        if (employeeIdExists) {
+          throw new Error("EmployeeId cannot have duplicate");
+        }
+      }),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username cannot be empty")
+      .bail()
+      .custom(async (username) => {
+        const usernameExists = await User.findOne({ username });
+        if (usernameExists) {
+          throw new Error("Username already taken");
+        }
+      }),
+    body("password")
+      .notEmpty()
+      .withMessage("Password cannot be empty")
+      .bail()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters long"),
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email cannot be empty")
+      .bail()
+      .isEmail()
+      .withMessage("Email is invalid")
+      .bail()
+      .custom(async (email) => {
+        const emailExists = await User.findOne({ email });
+        if (emailExists) {
+          throw new Error("Email already in use");
+        }
+      }),
+
+    body("lastName").trim().notEmpty().withMessage("Last Name cannot be empty"),
+    body("firstName")
+      .trim()
+      .notEmpty()
+      .withMessage("First Name cannot be empty"),
+    body("role")
+      .trim()
+      .notEmpty()
+      .withMessage("Role cannot be empty")
+      .bail()
+      .isIn(validRoles)
+      .withMessage("Invalid role"),
+    body("status")
+      .trim()
+      .notEmpty()
+      .withMessage("Status cannot be empty")
+      .bail()
+      .isIn(validStatuses)
+      .withMessage("Invalid status"),
+  ],
+};
+
+module.exports = authValidator;
