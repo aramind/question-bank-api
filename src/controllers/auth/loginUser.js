f870d2111a9cf3dd7087bf379546f573c08cfe19
aramind <88814622+aramind@users.noreply.github.com>
2024-05-04 00:04:12 +0800

    refactor: restructuring the return of login controller
        - to be in sync with the implementation of log in in the fe

--- src/controllers/auth/loginUser.js
+++ src/controllers/auth/loginUser.js
@@ -16,13 +16,12 @@ const loginUser = async (req, res) => {
     const result = validationResult(req);
     console.log(result.array());
     if (!result.isEmpty()) {
-      sendResponse.error(
+      return sendResponse.error(
         res,
         result.array(),
         getAllErrorMessages(result.array()),
         400
       );
-      return;
     }
 
     console.log(matchedData(req));
@@ -30,14 +29,12 @@ const loginUser = async (req, res) => {
 
     const foundUser = await User.findOne({ username });
     if (!foundUser) {
-      sendResponse.failed(res, "Invalid credentials", null, 401);
-      return;
+      return sendResponse.failed(res, "Invalid credentials", null, 401);
     }
 
     const match = await comparePassword(password, foundUser.password);
     if (!match) {
-      sendResponse.failed(res, "Invalid credentials", null, 401);
-      return;
+      return sendResponse.failed(res, "Invalid credentials", null, 401);
     } else {
       // create and attach jwt tokens
       const accessToken = generateAccessToken(foundUser);
@@ -51,7 +48,25 @@ const loginUser = async (req, res) => {
         // secure: true,
         maxAge: 24 * 60 * 60 * 1000,
       });
-      sendResponse.success(res, "Login Successful", accessToken, 200);
+
+      const returnedUserInfo = _.pick(foundUser, [
+        "_id",
+        "name",
+        "username",
+        "role",
+        "status",
+        "employeeId",
+      ]);
+
+      sendResponse.success(
+        res,
+        "Login Successful",
+        {
+          ...returnedUserInfo,
+          token: accessToken,
+        },
+        200
+      );
     }
   } catch (error) {
     console.error("Error logging in", error);
